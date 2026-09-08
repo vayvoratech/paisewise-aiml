baf8d4640a8b07860b20f079b8e73f2d5ad6f1e8
Ganesh Sesha Sai Akhil <akhil.devhub@gmail.com>
2026-09-08 21:54:43 +0530

    Update embeddings.py

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,12 +1,24 @@
 from sentence_transformers import SentenceTransformer
 
 
-# Load the embedding model
-model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+# Model is loaded only when create_embedding() is called
+model = None
+
+
+def get_model():
+    global model
+
+    if model is None:
+        model = SentenceTransformer(
+            "paraphrase-multilingual-MiniLM-L12-v2"
+        )
+
+    return model
 
 
 def create_embedding(text):
+    embedding_model = get_model()
 
-    embedding = model.encode(text)
+    embedding = embedding_model.encode(text)
 
     return embedding.tolist()
